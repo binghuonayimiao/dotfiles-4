b5d3456c1710c881f14ae4cc079e30ae90dba4e0
Salvydas Lukosius <59910950+ss-o@users.noreply.github.com>
2021-01-07 00:43:29 +0000

    a

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,4 +1,4 @@
-alias get-p10k="source $HOME/$DOTFILES/zsh/config/profile/10k.profile.zsh"
+alias get-p10k="source $HOME/$DOTFILES/zsh/config/profile/p10k.profile.zsh"
 alias get-spaceship="source $HOME/$DOTFILES/zsh/config/profile/spaceship.profile.zsh"
 alias get-ohmyzsh="source $HOME/$DOTFILES/zsh/config/profile/ohmyzsh.profile.zsh"
 alias get-local="source $HOME/$DOTFILES/zsh/config/profile/local.profile.zsh"
