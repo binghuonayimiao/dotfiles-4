76c915d2b4a209245a899ba75ae9457ce429a80a
Salvydas Lukosius <59910950+ss-o@users.noreply.github.com>
2021-01-11 18:06:38 +0000

    modified:   zsh/config/p10k/p10k-robby-by-sall.zsh

--- zsh/config/p10k/p10k-robby-by-sall.zsh
+++ zsh/config/p10k/p10k-robby-by-sall.zsh
@@ -78,7 +78,7 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    # time                  # current time
+    time                  # current time
     status                  # exit code of the last command
     # =========================[ Line #2 ]=========================
     # newline                 # \n
